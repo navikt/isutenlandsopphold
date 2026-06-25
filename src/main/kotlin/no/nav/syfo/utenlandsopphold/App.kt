cd9996fc86b2d271f2f9921ceb807789c2ec4dca
Daniel Engebretsen <daniel.engebretsen@nav.no>
2026-06-25 09:58:03 +0200

    bump ktlint and shadow versions

--- src/main/kotlin/no/nav/syfo/utenlandsopphold/App.kt
+++ src/main/kotlin/no/nav/syfo/utenlandsopphold/App.kt
@@ -7,31 +7,33 @@ import no.nav.syfo.utenlandsopphold.api.apiModule
 import no.nav.syfo.utenlandsopphold.application.ApplicationState
 
 fun main(args: Array<String>) {
-    val applicationState = ApplicationState(
-        alive = true,
-        ready = true
-    )
-    val server = embeddedServer(
-        Netty,
-        configure = {
-            connector {
-                port = 8080
-            }
-            connectionGroupSize = 8
-            workerGroupSize = 8
-            callGroupSize = 16
-        },
-        module = {
-            apiModule(
-                applicationState = applicationState
-            )
-        }
-    )
+    val applicationState =
+        ApplicationState(
+            alive = true,
+            ready = true,
+        )
+    val server =
+        embeddedServer(
+            Netty,
+            configure = {
+                connector {
+                    port = 8080
+                }
+                connectionGroupSize = 8
+                workerGroupSize = 8
+                callGroupSize = 16
+            },
+            module = {
+                apiModule(
+                    applicationState = applicationState,
+                )
+            },
+        )
 
     Runtime.getRuntime().addShutdownHook(
         Thread {
             applicationState.ready = false
-        }
+        },
     )
 
     server.start(wait = true)
--- src/main/kotlin/no/nav/syfo/utenlandsopphold/api/ApiModule.kt
+++ src/main/kotlin/no/nav/syfo/utenlandsopphold/api/ApiModule.kt
@@ -5,12 +5,10 @@ import io.ktor.server.routing.routing
 import no.nav.syfo.utenlandsopphold.api.endpoints.registerPodApi
 import no.nav.syfo.utenlandsopphold.application.ApplicationState
 
-fun Application.apiModule(
-    applicationState: ApplicationState
-) {
+fun Application.apiModule(applicationState: ApplicationState) {
     routing {
         registerPodApi(
-            applicationState = applicationState
+            applicationState = applicationState,
         )
     }
 }
