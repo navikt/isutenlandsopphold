48475dfc034c27821622b7fcc2ab133e84c5ff33
Daniel Engebretsen <daniel.engebretsen@nav.no>
2026-06-29 13:23:17 +0200

    adjust flyway script

--- src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
+++ src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
@@ -1,14 +1,5 @@
 DO $$
 BEGIN
-    REVOKE ALL ON ALL TABLES IN SCHEMA public FROM cloudsqliamuser;
-    GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
-    EXCEPTION WHEN undefined_object THEN
-    RAISE NOTICE 'role cloudsqliamuser does not exist, skipping grants';
-END
-$$;
-
-DO $$
-BEGIN
     GRANT SELECT ON ALL TABLES IN SCHEMA public TO "isyfo-analyse";
     EXCEPTION WHEN undefined_object THEN
     RAISE NOTICE 'role isyfo-analyse does not exist, skipping grants';
