ee6851910a4fb4f473e8b3db3f43031b245f26f8
Daniel Engebretsen <daniel.engebretsen@nav.no>
2026-07-03 09:07:19 +0200

    remove import

--- src/main/kotlin/no/nav/syfo/utenlandsopphold/application/ApplicationState.kt
+++ src/main/kotlin/no/nav/syfo/utenlandsopphold/application/ApplicationState.kt
@@ -1,7 +1,5 @@
 package no.nav.syfo.utenlandsopphold.application
 
-import kotlin.concurrent.Volatile
-
 data class ApplicationState(
     @Volatile var alive: Boolean = true,
     @Volatile var ready: Boolean = false,
