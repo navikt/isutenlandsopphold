1172e35cfd9b919e17b6cfe08a867674524d8b3d
Daniel Engebretsen <daniel.engebretsen@nav.no>
2026-07-15 13:54:01 +0200

    fix login for isyfo analyse (#39)

--- src/main/resources/db/migration/R__grant_access_to_tables.sql
+++ src/main/resources/db/migration/R__grant_access_to_tables.sql
@@ -8,11 +8,13 @@ $$;
 
 DO $$
 BEGIN
-CREATE ROLE "isyfo-analyse" WITH NOLOGIN;
+CREATE ROLE "isyfo-analyse" WITH LOGIN;
 EXCEPTION WHEN DUPLICATE_OBJECT THEN
   RAISE NOTICE 'not creating role isyfo-analyse -- it already exists';
 END
 $$;
+
+ALTER ROLE "isyfo-analyse" WITH LOGIN;
 
 REVOKE ALL ON ALL TABLES IN SCHEMA public FROM cloudsqliamuser;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
