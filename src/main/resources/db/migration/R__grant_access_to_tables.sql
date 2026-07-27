91465d98ca462a913804dd293628a4140d95491b
geir-waagboe <geir.arne.waagbo@nav.no>
2026-07-27 11:58:40 +0200

    Bump dependencies

--- src/main/resources/db/migration/R__grant_access_to_tables.sql
+++ src/main/resources/db/migration/R__grant_access_to_tables.sql
@@ -1,21 +1,3 @@
-DO $$
-BEGIN
-CREATE ROLE cloudsqliamuser WITH NOLOGIN;
-EXCEPTION WHEN DUPLICATE_OBJECT THEN
-  RAISE NOTICE 'not creating role cloudsqliamuser -- it already exists';
-END
-$$;
-
-DO $$
-BEGIN
-CREATE ROLE "isyfo-analyse" WITH LOGIN;
-EXCEPTION WHEN DUPLICATE_OBJECT THEN
-  RAISE NOTICE 'not creating role isyfo-analyse -- it already exists';
-END
-$$;
-
-ALTER ROLE "isyfo-analyse" WITH LOGIN;
-
 REVOKE ALL ON ALL TABLES IN SCHEMA public FROM cloudsqliamuser;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO "isyfo-analyse";
