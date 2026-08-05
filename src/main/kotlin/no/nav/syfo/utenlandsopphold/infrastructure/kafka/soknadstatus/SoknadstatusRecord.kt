214ca28b0dae96c366567af73c36a5309e14c777
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2026-08-05 12:55:58 +0200

    IS-4123: Kafka-topic for vedtak (#54)

---
+++ src/main/kotlin/no/nav/syfo/utenlandsopphold/infrastructure/kafka/soknadstatus/SoknadstatusRecord.kt
@@ -0,0 +1,76 @@
+package no.nav.syfo.utenlandsopphold.infrastructure.kafka.soknadstatus
+
+import no.nav.syfo.utenlandsopphold.domain.Soknad
+import no.nav.syfo.utenlandsopphold.domain.Utfall
+import java.time.LocalDate
+import java.time.OffsetDateTime
+import java.util.UUID
+
+data class SoknadstatusRecord(
+    val uuid: UUID,
+    val createdAt: OffsetDateTime,
+    val personident: String,
+    val status: Soknadstatus,
+    val vedtak: VedtakRecord? = null,
+) {
+    companion object {
+        fun fromSoknad(soknad: Soknad) =
+            SoknadstatusRecord(
+                uuid = soknad.eksternId,
+                createdAt = soknad.innsendtTidspunkt,
+                personident = soknad.personident.value,
+                status = Soknadstatus.MOTTATT,
+            )
+
+        fun fromSoknadMedVedtak(soknad: Soknad): SoknadstatusRecord {
+            require(soknad.vedtak != null) {
+                "Soknad må ha vedtak for å lage SoknadstatusRecord med vedtak"
+            }
+            return SoknadstatusRecord(
+                uuid = soknad.eksternId,
+                createdAt = soknad.innsendtTidspunkt,
+                personident = soknad.personident.value,
+                status = Soknadstatus.BEHANDLET,
+                vedtak =
+                    VedtakRecord(
+                        uuid = soknad.vedtak.vedtakId,
+                        createdAt = soknad.vedtak.fattetTidspunkt,
+                        veilederident = soknad.vedtak.fattetAv.value,
+                        utfall = soknad.vedtak.utfall.toVedtakRecordUtfall(),
+                        innvilgedePerioder = soknad.vedtak.innvilgedePerioder.map { VedtakRecordPeriode(it.fom, it.tom) },
+                    ),
+            )
+        }
+    }
+}
+
+enum class Soknadstatus {
+    MOTTATT,
+    BEHANDLET,
+}
+
+data class VedtakRecord(
+    val uuid: UUID,
+    val createdAt: OffsetDateTime,
+    val veilederident: String,
+    val utfall: VedtakRecordUtfall,
+    val innvilgedePerioder: List<VedtakRecordPeriode>,
+)
+
+enum class VedtakRecordUtfall {
+    AVSLAG,
+    DELVIS_INNVILGET,
+    INNVILGET,
+}
+
+data class VedtakRecordPeriode(
+    val fom: LocalDate,
+    val tom: LocalDate,
+)
+
+private fun Utfall.toVedtakRecordUtfall(): VedtakRecordUtfall =
+    when (this) {
+        is Utfall.Avslag -> VedtakRecordUtfall.AVSLAG
+        is Utfall.DelvisInnvilget -> VedtakRecordUtfall.DELVIS_INNVILGET
+        is Utfall.Innvilget -> VedtakRecordUtfall.INNVILGET
+    }
